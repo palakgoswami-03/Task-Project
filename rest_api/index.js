88b1226c82c6c7f8b6329437b4b47d9693e283a4
palak-goswami <pg8718809@gmail.com>
2025-08-22 17:24:23 +0530

    done

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const users = require("./MOCK_DATA.json");
-
+const fs = require("fs");
 const app = express();
 const PORT = 8000;
+app.use(express.urlencoded({ extended : false}));
 //ROUTES
 app.get("/api/users",(req,res)=>{
     return res.json(users);
@@ -24,11 +25,17 @@ app
     return res.json(user);
 })
 .patch((req,res) =>{
-    res.json({ status: "pending"});
+    return res.json({ status: "pending"});
 })
 .delete((req,res)=>{
-    res.json({status: "pending"});
+    const body = req.body;
+    users.delete({...body,id:users.length});
+    fs.unlink('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+    return res.json({ status:"success",id: users.length});
+    })
+    console.log('body',body)
 });
+
 
 
 // app.get('/api/users/:id',(req,res)=>{
@@ -37,10 +44,16 @@ app
 //     return res.json(user);
 // });
 
-// app.post('/api/users',(req,res)=>{
-//     //tood:create new user
-//     return res.json({ status:"pending"});
-// })
+app.post('/api/users',(req,res)=>{
+    // tood:create new user
+    const body = req.body;
+    users.push({...body,id:users.length + 1});
+    fs.writeFile('./MOCK_DATA.json',JSON.stringify(users),(err,data)=>{
+    return res.json({ status:"success",id: users.length});
+    })
+    console.log('body',body)
+    
+})
 
 // app.patch("/api/users/:id",(req,res)=>{
 // //tood: edit the user with id
